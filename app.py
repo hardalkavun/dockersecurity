080430c5c6a540f5a20b95a70d09c9ddd804394b
Eren Kaptan <eren.kaptan@gazi.edu.tr>
2026-04-23 21:10:03 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+Docker Security Dashboard
+3bbabb23785e
+
+Image: nginx
+
+Risk: MEDIUM
+
+Score: 70
+
+    Running as root
+    Root filesystem is writable
+    Common service image - check CVEs
+
+23f51c652279
+
+Image: kalilinux/kali-rolling
+
+Risk: HIGH
+
+Score: 80
+
+    Running as root
+    Root filesystem is writable
+
+fa23883656a0
+
+Image: kalilinux/kali-rolling
+
+Risk: HIGH
+
+Score: 80
+
+    Running as root
+    Root filesystem is writable
+
+9902738f9c29
+
+Image: memgraph/memgraph-platform:latest
+
+Risk: HIGH
+
+Score: 90
+
+    Running as root
+    Root filesystem is writable
+    Unpinned image version (latest)
